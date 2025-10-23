7a5a6215f5ee304bf96c7f8dcfb6219f8b6ecb5c
arkanlight <kristhianaguasaco@gmail.com>
2025-10-22 20:55:55 -0500

    links

--- TABLERO_ADMINISTRATIVO/Admon/aspecto_academico.php
+++ TABLERO_ADMINISTRATIVO/Admon/aspecto_academico.php
@@ -73,7 +73,7 @@ $result = $conn->query("SELECT * FROM aspectos_academicos ORDER BY id DESC");
 
 <div id="wrapper">
     <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+ <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
@@ -153,7 +153,7 @@ $result = $conn->query("SELECT * FROM aspectos_academicos ORDER BY id DESC");
 
     <hr class="sidebar-divider">
 
-</ul>
+ </ul>
 <!-- Fin Sidebar -->
 
 
